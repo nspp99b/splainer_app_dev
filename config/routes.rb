d1b423cde7891e0b101950727ba304638d1c1fc9
nspp99b <chromepanthers@gmail.com>
2017-12-28 00:46:17 -0500

    added signup and login functions, user controller, user new and create views, shared view for errors, styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home' #(creates root_path)
   get '/help', to: 'static_pages#help' #(help_path)
   get '/about', to: 'static_pages#about' #(about_path)
   get '/contact', to: 'static_pages#contact' #(contact_path)
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
